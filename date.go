c3bdb3c4ea7e9d7a39f70824fc8646779e09bf6d
young-win <young.zhu.2012@gmail.com>
2023-10-16 15:27:05 +0800

    Date() & Datetime()

--- date.go
+++ date.go
@@ -1,9 +1,36 @@
 package go2chinese
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 const (
-	// Sunday ... long date names
+	layoutDate           = "2006年1月2日"
+	layoutDatetime       = "2006年1月2日 15时4分5秒"
+	layoutDatetimeHour12 = "2006年1月2日 3时4分5秒"
+)
+
+func Date(t time.Time) string {
+	return t.Format(layoutDate)
+}
+
+func Datetime(t time.Time) string {
+	switch t.Hour() {
+	case 0:
+		// 处理  00时
+		s := t.Format(layoutDatetime)
+		return strings.ReplaceAll(s, " 00", " 0")
+	case 1, 2, 3, 4, 5, 6, 7, 8, 9:
+		return t.Format(layoutDatetimeHour12)
+	default:
+		return t.Format(layoutDatetime)
+	}
+}
+
+// week
+const (
+	// Sunday ... long weekday names
 	Sunday    = "星期天"
 	Monday    = "星期一"
 	Tuesday   = "星期二"
@@ -12,7 +39,7 @@ const (
 	Friday    = "星期五"
 	Saturday  = "星期六"
 
-	// SundayShort ... short date names
+	// SundayShort ... short weekday names
 	SundayShort    = "周日"
 	MondayShort    = "周一"
 	TuesdayShort   = "周二"
--- date_test.go
+++ date_test.go
@@ -1,9 +1,51 @@
 package go2chinese
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
+
+func TestDate(t *testing.T) {
+	testcases := []struct {
+		date time.Time
+		want string
+	}{
+		{newDate(2023, 10, 13), "2023年10月13日"},
+		{newDate(2023, 9, 1), "2023年9月1日"},
+	}
+	for _, testcase := range testcases {
+		t.Run("", func(t *testing.T) {
+			got := Date(testcase.date)
+			if got != testcase.want {
+				t.Errorf("want: %s, but got: %s", testcase.want, got)
+			}
+		})
+	}
+}
+
+func TestDatetime(t *testing.T) {
+	fmt.Println(2 << 8)
+	testcases := []struct {
+		datetime time.Time
+		want     string
+	}{
+		{newDatetime(2023, 10, 13, 0, 0, 0), "2023年10月13日 0时0分0秒"},
+		{newDatetime(2023, 10, 13, 1, 59, 59), "2023年10月13日 1时59分59秒"},
+		{newDatetime(2023, 10, 13, 1, 9, 59), "2023年10月13日 1时9分59秒"},
+		{newDatetime(2023, 10, 13, 23, 59, 59), "2023年10月13日 23时59分59秒"},
+		{newDatetime(2006, 1, 2, 0, 0, 0), "2006年1月2日 0时0分0秒"},
+		{newDatetime(2006, 1, 2, 15, 4, 5), "2006年1月2日 15时4分5秒"},
+	}
+	for _, testcase := range testcases {
+		t.Run("", func(t *testing.T) {
+			got := Datetime(testcase.datetime)
+			if got != testcase.want {
+				t.Errorf("want: %q, but got: %q", testcase.want, got)
+			}
+		})
+	}
+}
 
 func TestWeekday(t *testing.T) {
 	testcases := []struct {
@@ -51,4 +93,12 @@ func TestShortWeekday(t *testing.T) {
 			}
 		})
 	}
+}
+
+func newDate(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+}
+
+func newDatetime(year int, month time.Month, day, hour, min, sec int) time.Time {
+	return time.Date(year, month, day, hour, min, sec, 0, time.Local)
 }
